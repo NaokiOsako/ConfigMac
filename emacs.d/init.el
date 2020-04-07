6cc3604a1f0b9ffff93d0916584684b5058ed0cf
NaokiOsako <naoki-osako@kwansei.ac.jp>
2020-04-07 18:46:49 +0900

    [fix]emacs.d

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -228,3 +228,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+;;; *.~ とかのバックアップファイルを作らない
+(setq make-backup-files nil)
+;;; .#* とかのバックアップファイルを作らない
+(setq auto-save-default nil)
